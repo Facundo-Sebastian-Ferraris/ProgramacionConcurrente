3a038f18a897f5a50334a8987c0af64f8cc42f39
Facundo-Sebastian-Ferraris <facundo.ferraris@est.fi.uncoma.edu.ar>
2025-12-13 14:10:30 -0300

    casi terminado confiteria

--- CentroSki/src/ElementosCentro/Confiteria.java
+++ CentroSki/src/ElementosCentro/Confiteria.java
@@ -1,29 +1,82 @@
 package ElementosCentro;
 
-import java.util.concurrent.Exchanger;
 import java.util.concurrent.Semaphore;
 
 public class Confiteria {
-    private final Semaphore GENERIC_Sillas;
-    private final Exchanger<Boolean> pedido;    
+    private final Semaphore GENERIC_Sillas = new Semaphore(100);    
+    private final Semaphore RENDEVOUZ_Cajero = new Semaphore(0);    
+    private final Semaphore RENDEVOUZ_TomarPedido = new Semaphore(0);    
+    private final Semaphore RENDEVOUZ_RecibirPedido = new Semaphore(0);    
+    private final Semaphore RENDEVOUZ_RecibirNumeroMesa = new Semaphore(0);    
+    private final Semaphore RENDEVOUZ_Cocina = new Semaphore(0);    
+    private final Semaphore RENDEVOUZ_Caja = new Semaphore(0);    
+    private final Semaphore[] Mesas = new Semaphore[3];    
+    
+   
+    
+    private int numeroMesa = 0;
+    private boolean conPostre = false;
     
 
 
     public Confiteria(){
-        GENERIC_Sillas = new Semaphore(100);
-        pedido = new Exchanger();
+        Mesas[0] = new Semaphore(0);
+        Mesas[1] = new Semaphore(0);
+        Mesas[2] = new Semaphore(0);
+
     }
 
     public void cliente_Ingresar(boolean conPostre) throws InterruptedException{
         GENERIC_Sillas.acquire(); //toma silla
 
-        pedido.exchange(conPostre);
+        RENDEVOUZ_Cajero.release();//notifica cajero
+        RENDEVOUZ_TomarPedido.acquire();//espero a dar su pedido
+        
+        //pedido
+        this.conPostre = conPostre;
+        
+        RENDEVOUZ_RecibirPedido.release();//notifica a cajero que termino su pedido
+        RENDEVOUZ_RecibirNumeroMesa.acquire();
+        int nMesa = numeroMesa;
+        
+        RENDEVOUZ_Caja.release();
+        
+        Mesas[nMesa].acquire();
+        if (conPostre) {
+            Mesas[2].acquire();
+        }
+        
+        Thread.sleep(3000);
+        
         GENERIC_Sillas.release(); //libera silla
-
     }
 
-     public void cajero_Atender(boolean conPostre) throws InterruptedException{
-        pedido.exchange(null);
+     public void cajero_Atender() throws InterruptedException{
+        RENDEVOUZ_Cajero.acquire();
+        RENDEVOUZ_TomarPedido.release();
+        RENDEVOUZ_RecibirPedido.acquire();
+        
+        numeroMesa = (numeroMesa + 1)%2;
+        
+        RENDEVOUZ_Cocina.release();
+        RENDEVOUZ_RecibirNumeroMesa.release();
+        
+        RENDEVOUZ_Caja.acquire(2);
         
     }
+     
+     public void cocinero_Preparar() throws InterruptedException{
+         RENDEVOUZ_Cocina.acquire();
+         
+         int nMesa = numeroMesa;
+         boolean postre = conPostre;
+         
+         RENDEVOUZ_Caja.release();
+         
+         Thread.sleep(3000);
+         Mesas[nMesa].release();
+         if (postre) {
+             Mesas[2].release();
+         }
+     }
 }
